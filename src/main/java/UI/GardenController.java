263d90946d454422e27c47e3b228b1a0a9a92eba
Sisi Huang <hsspig@gmail.com>
2024-05-27 09:53:12 -0700

    Add planting logic in UI

--- src/main/java/UI/GardenController.java
+++ src/main/java/UI/GardenController.java
@@ -8,31 +8,50 @@ import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.image.Image;
 import javafx.scene.ImageCursor;
 import javafx.scene.Cursor;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
+import javafx.scene.paint.Color;
 
+
+enum Mode { // Enum to represent the current mode of the garden controller
+    WATERING, PLANTING, NONE
+}
 
 public class GardenController {
     @FXML
-    private Group soilGroup;
+    private Group soilGroup, plantGroup;
     @FXML
-    private Button waterButton, rainButton, plantButton, exitButton;
+    private Button waterButton, rainButton, plantButton, // for watering, raining, and planting
+                   cancelButton, confirmButton, // for canceling and confirming planting
+                   closeButton; // for closing the popup
+    @FXML
+    private Label plantTypeValue, plantNumberValue, humidityValue, temperatureValue, attackStatusValue, healthStatusValue;
     @FXML
     private Pane rainPane, overlayPane;
     @FXML
-    private AnchorPane popupPane;
+    private AnchorPane popupSelectionPane, popupStatusPane;
     @FXML
     private ImageView sunny;
+    @FXML
+    private Spinner<Integer> seedQuantitySpinner;
+    @FXML
+    private GridPane seedSelectionGrid;
 
-    private boolean isWateringMode = false;// Flag to track watering mode, which is toggled by the water button.
-    private boolean isWatering = false;  // Flag to track watering state
+    //private boolean isWateringMode = false;// Flag to track watering mode, which is toggled by the water button.
+    private Mode currentMode = Mode.NONE;  // The current mode of the garden controller
+    private String currentSeedType;  // The type of seed currently selected;
 
     private Image normalSoil = new Image(getClass().getResourceAsStream("/image/soil/3.png"));
     private Image wetSoil = new Image(getClass().getResourceAsStream("/image/soil/4.png"));
@@ -42,30 +61,143 @@ public class GardenController {
 
     @FXML
     private void initialize() {
-        // Optionally initialize something
+        // Configure the spinner for integer values with a min, max, and step
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100, 1);
+        seedQuantitySpinner.setValueFactory(valueFactory);
     }
 
+    /**
+     * Sets the visibility and managed state of a given JavaFX Node.
+     *
+     * @param node The JavaFX Node to set visibility and managed state.
+     * @param isVisible boolean to set specific visibility. If true, the node will be visible and managed, otherwise hidden.
+     */
+    private void setNodeVisibility(Node node, Boolean isVisible) {
+        node.setVisible(isVisible);
+        node.setManaged(isVisible);
+    }
+
+    /**
+     * Handles the click event on the water button.
+     */
     @FXML
     private void handleWaterButtonClick() {
-        isWateringMode = !isWateringMode;  // Toggle watering mode
-        if (isWateringMode) {
+        // Toggle watering mode
+        currentMode = (currentMode == Mode.WATERING) ? Mode.NONE : Mode.WATERING;
+        if (currentMode == Mode.WATERING) {
             // Change cursor to indicate watering mode
             soilGroup.getScene().setCursor(new ImageCursor(new Image(getClass().getResourceAsStream("/image/icon/water_cursor.png")), 0, 0));
+            plantButton.setDisable(true);  // Disable the plant button
+            rainButton.setDisable(true);  // Disable the rain button
         } else {
             // Reset cursor to default when disabling watering mode
             soilGroup.getScene().setCursor(Cursor.DEFAULT);
+            // TODO: Add logic to handle watering mode deactivation, might be needed to disable more buttons
+            plantButton.setDisable(false);  // Enable the plant button
+            rainButton.setDisable(false);  // Enable the rain button
         }
     }
 
+    /**
+     * Handles the watering of a soil when clicked.
+     * @param event The MouseEvent that triggered the watering.
+     */
+    private void handleWatering(MouseEvent event) {
+        if (currentMode != Mode.WATERING) {
+            return;
+        }
+
+        ImageView clickedSoil = (ImageView) event.getSource();
+        clickedSoil.setImage(wetSoil);
+
+        //TODO: Add logic to handle watering of the soil
+    }
+
+    /**
+     * Handles the planting of a seed when clicked.
+     * @param event The MouseEvent that triggered the planting.
+     */
+    private void handlePlanting(MouseEvent event) {
+        if (currentSeedType == null) {
+            return;
+        }
+
+        ImageView clickedSoil = (ImageView) event.getSource();
+        String soilId = clickedSoil.getId();
+        ImageView plantImageView = (ImageView) plantGroup.lookup("#" + soilId);
+        if (plantImageView != null && plantImageView.getImage() == null) {
+            int seedQuantity = seedQuantitySpinner.getValue();
+            plantImageView.setImage(new Image(getClass().getResourceAsStream("/image/plants/" + currentSeedType + ".png")));
+
+            //TODO: Add logic to handle planting of the seed
+            System.out.println("Planting " + seedQuantity + " " + currentSeedType + " seed" +
+                    ((seedQuantity > 1) ? "s" : ""));
+        }
+    }
+
+    /**
+     * Sets the values of the plant status labels.
+     * @param plantType The type of the plant.
+     * @param plantNumber The number of the plant.
+     * @param humidity The humidity of the plant.
+     * @param temperature The temperature of the plant.
+     * @param attackStatus The attack status of the plant.
+     * @param healthStatus The health status of the plant.
+     */
+    private void setLabelValues(String plantType, String plantNumber, String humidity, String temperature, String attackStatus, String healthStatus) {
+        plantTypeValue.setText(plantType);
+        plantNumberValue.setText(plantNumber);
+        humidityValue.setText(humidity);
+        temperatureValue.setText(temperature);
+        attackStatusValue.setText(attackStatus);
+        healthStatusValue.setText(healthStatus);
+    }
+
+    /**
+     * Shows the information of a soil when clicked.
+     * @param event The MouseEvent that triggered the click.
+     */
+    private void showSoilInfo(MouseEvent event) {
+        ImageView clickedSoil = (ImageView) event.getSource();
+        String soilId = clickedSoil.getId();
+        ImageView plantImageView = (ImageView) plantGroup.lookup("#" + soilId);
+        if (plantImageView != null && plantImageView.getImage() != null) {
+            String plantType = plantImageView.getId();
+            //TODO: Add logic to get the plant number, humidity, temperature, attack status, and health status
+
+        } else {
+            setLabelValues("N/A", "N/A", "N/A", "N/A", "N/A", "N/A");
+        }
+        setNodeVisibility(popupStatusPane, true);
+        setNodeVisibility(overlayPane, true);
+    }
+
+    /**
+     * Handles the click event on a soil.
+     * @param event The MouseEvent that triggered the click.
+     */
     @FXML
     private void handleSoilClick(MouseEvent event) {
-        if (isWateringMode) {
-            ImageView clickedSoil = (ImageView) event.getSource();
-            clickedSoil.setImage(wetSoil);
+        switch (currentMode) {
+            case WATERING:
+                handleWatering(event);
+                break;
+            case PLANTING:
+                handlePlanting(event);
+                currentMode = Mode.NONE;
+                break;
+            case NONE:
+                showSoilInfo(event);
+                break;
+
         }
 
     }
 
+    /**
+     * Sets all soils to a specific image.
+     * @param soilImage The image to set all soils to.
+     */
     private void setAllSoils(Image soilImage) {
         for (Node node : soilGroup.getChildren()) {
             if (node instanceof ImageView) {
@@ -75,6 +207,9 @@ public class GardenController {
         }
     }
 
+    /**
+     * Handles the click event on the rain button.
+     */
     @FXML
     protected void handleRainButtonClick() {
         if(weather.isSunny()) {
@@ -90,9 +225,15 @@ public class GardenController {
         // Reset cursor to default every time the rain button is clicked
         soilGroup.getScene().setCursor(Cursor.DEFAULT);
         // Also, ensure watering mode is deactivated when rain toggled
-        isWateringMode = false;
+        currentMode = Mode.NONE;
     }
 
+    /**
+     * Animates the sunny image to appear or disappear.
+     * @param fromScale The starting scale of the image.
+     * @param toScale The ending scale of the image.
+     * @param setVisibleAfter Whether to set the image to invisible after the animation.
+     */
     private void animateSunnyImage(double fromScale, double toScale, boolean setVisibleAfter) {
         sunny.setVisible(true);
 
@@ -112,6 +253,10 @@ public class GardenController {
         scaleTransition.play();
     }
 
+    /**
+     * Creates a raindrop animation on the given pane.
+     * @param pane The Pane to create the raindrop animation on.
+     */
     private void createRaindrop(Pane pane) {
         for (int i = 0; i < 100; i++) {  // Number of raindrops
             Rectangle drop = new Rectangle(2, 20);  // Creating a drop as a small rectangle
@@ -130,20 +275,74 @@ public class GardenController {
         }
     }
 
+    /**
+     * Resets the style of all seed buttons to default.
+     */
+    private void resetSeedButtonStyle() {
+        for (Node node : seedSelectionGrid.getChildren()) {
+            if (node instanceof Button) {
+                Button seedButton = (Button) node;
+                seedButton.setEffect(null);
+            }
+        }
+    }
+
+    /**
+     * Handles the click event on the plant button.
+     */
     @FXML
     private void handlePlantButtonClick(){
-        boolean isVisible = popupPane.isVisible();
-        popupPane.setVisible(!isVisible);
-        popupPane.setManaged(!isVisible);
-        overlayPane.setVisible(!isVisible);
-        overlayPane.setManaged(!isVisible);
+        currentMode = Mode.PLANTING;
+        setNodeVisibility(popupSelectionPane, true);
+        setNodeVisibility(overlayPane, true);
+        resetSeedButtonStyle();
+    }
 
+    /**
+     * Handles the click event on a seed selection.
+     */
+    @FXML
+    private void handleSeedSelection(MouseEvent event) {
+        resetSeedButtonStyle();
+        // Extract which seed was selected
+        Button selectedSeedButton = (Button) event.getSource();
+        selectedSeedButton.setEffect(new DropShadow(10, Color.BLACK));
+        currentSeedType = selectedSeedButton.getText();
+
+        // Configure and show the quantity spinner
+        seedQuantitySpinner.getValueFactory().setValue(1); // Reset to default value
+        setNodeVisibility(seedQuantitySpinner, true);
+        setNodeVisibility(confirmButton, true);
+    }
+
+    /**
+     * Handles the click event on the confirm planting button.
+     */
+    @FXML
+    private void handleConfirmPlanting(){
+        setNodeVisibility(seedQuantitySpinner, false);
+        setNodeVisibility(confirmButton, false);
+        popupSelectionPane.setVisible(false);
+        overlayPane.setVisible(false);
+    }
+
+    /**
+     * Handles the click event on the cancel planting button.
+     */
+    @FXML
+    private void handleCancelPlanting(){
+        currentMode = Mode.NONE;
+        setNodeVisibility(seedQuantitySpinner, false);
+        setNodeVisibility(confirmButton, false);
+        setNodeVisibility(popupSelectionPane, false);
+        setNodeVisibility(overlayPane, false);
     }
 
     @FXML
-    private void handleExitButtonClick(){
-        popupPane.setVisible(false);
-        overlayPane.setVisible(false);
+    private void handleCloseButtonClick() {
+        setNodeVisibility(popupStatusPane, false);
+        setNodeVisibility(overlayPane, false);
     }
+
 
 }
