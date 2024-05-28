9f11ef739ca4c882c59421611e7a9535cbc3dec5
Szekim <szekim@outlook.com>
2024-05-28 01:42:34 -0700

    Update TemperatureController

--- src/main/java/controllers/TemperatureController.java
+++ src/main/java/controllers/TemperatureController.java
@@ -31,12 +31,10 @@ public class TemperatureController {
 
         if(currentTemperature > HIGH_TEMPERATURE_THRESHOLD){ // The temperature is too high
             System.out.println("Cooling down ...");
-            // Adjust the temperature to the optimal level
-            this.temperatureSensor.updateTemperature(OPTIMAL_TEMPERATURE);
+            temperatureToOptimal();
         }else if(currentTemperature < LOW_TEMPERATURE_THRESHOLD){
             System.out.println("Heating up ...");
-            // Adjust the temperature to the optimal level
-            this.temperatureSensor.updateTemperature(OPTIMAL_TEMPERATURE);
+            temperatureToOptimal();
         }else{
             System.out.println("The temperature is quit suitable for plants' growth!");
         }
@@ -49,5 +47,27 @@ public class TemperatureController {
      */
     private void logCurrentTemperature(double temperature){
         System.out.println("The current real-time temperature is " + temperature + " degrees");
+    }
+
+    /**
+     * Adjust the temperature to the optimal level
+     */
+    private void temperatureToOptimal() {
+        double currentTemperature = this.temperatureSensor.getTemperature();
+        if (currentTemperature > OPTIMAL_TEMPERATURE) {
+            while (currentTemperature > OPTIMAL_TEMPERATURE) {
+                // Simulate cooling down
+                currentTemperature -= 1.0;
+                System.out.println("Cooling: Current temperature is " + currentTemperature + " degrees.");
+            }
+        } else if (currentTemperature < OPTIMAL_TEMPERATURE) {
+            while (currentTemperature < OPTIMAL_TEMPERATURE) {
+                // Simulate heating up
+                currentTemperature += 1.0;
+                System.out.println("Heating: Current temperature is " + currentTemperature + " degrees.");
+            }
+        }
+        this.temperatureSensor.updateTemperature(OPTIMAL_TEMPERATURE);
+        System.out.println("Temperature adjusted to optimal level: " + OPTIMAL_TEMPERATURE + " degrees.");
     }
 }
