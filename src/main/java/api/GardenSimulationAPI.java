49ecce26b76e78108e104f8a5032f6204733603e
Sisi Huang <hsspig@gmail.com>
2024-06-02 23:15:16 -0700

    Add GardenSimulationAPI and GardenSimulator

---
+++ src/main/java/api/GardenSimulationAPI.java
@@ -0,0 +1,55 @@
+package api;
+
+import application.GardenManager;
+import plant.Plant;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GardenSimulationAPI {
+    private GardenManager gardenManager;
+
+    public GardenSimulationAPI() {
+        //gardenManager = new GardenManager("src/main/files/garden_config.txt");
+    }
+
+    public void initializeGarden() {
+        gardenManager.initializeGarden();
+    }
+
+    public Map<String, Object> getPlants() {
+        Map<String, Object> result = new HashMap<>();
+        List<List<Plant>> plantGroups = gardenManager.getPlantGroups();
+        List<String> plants = new ArrayList<>();
+        List<Double> waterRequirement = new ArrayList<>();
+        List<List<String>> parasites = new ArrayList<>();
+
+        for (List<Plant> plantGroup : plantGroups) {
+            if (!plantGroup.isEmpty()) {
+                plants.add(plantGroup.getFirst().getName());
+                waterRequirement.add(plantGroup.getFirst().getMinWaterLevel()); // TODO: Check if this is correct
+                List<String> plantParasites = plantGroup.getFirst().getPestList();
+                parasites.add(plantParasites);
+            }
+        }
+        return result;
+    }
+
+    public void rain(int amount) {
+
+    }
+
+    public void temperature(int temperature) {
+
+    }
+
+    public void parasite(String parasite) {
+
+    }
+
+    public void getState() {
+
+    }
+}
---
+++ src/main/java/api/GardenSimulator.java
@@ -0,0 +1,7 @@
+package api;
+
+public class GardenSimulator {
+    public static void main(String[] args) {
+        GardenSimulationAPI gardenSimulationAPI = new GardenSimulationAPI();
+    }
+}
