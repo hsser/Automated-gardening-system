d6eb1d82415d12ac6088a7446fe3ddcc6f330a33
Szekim <Szekim@outlook.com>
2024-06-02 23:06:36 -0700

    Add pest list in each type of plants

--- src/main/java/plant/PlantType.java
+++ src/main/java/plant/PlantType.java
@@ -1,23 +1,29 @@
 package plant;
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
 public enum PlantType {
-    TREE(20.0, 100.0, 30.0, 68.0, 86.0),
-    FLOWER(10.0, 50.0, 20.0, 68.0, 86.0),
-    CROP(30.0, 80.0, 40.0, 68.0, 86.0);
+    TREE(20.0, 100.0, 30.0, 32.0, 122.0, new ArrayList<>(Arrays.asList("Aphid", "Spider"))),
+    FLOWER(10.0, 50.0, 20.0, 32.0, 122.0, new ArrayList<>(Arrays.asList("Aphid", "Spider"))),
+    CROP(30.0, 80.0, 40.0, 32.0, 122.0, new ArrayList<>(Arrays.asList("Aphid", "Spider", "Whitefly")));
 
     private final double minWaterLevel;
     private final double maxWaterLevel;
     private final double lowWaterThreshold;
     private final double minTemperature;
     private final double maxTemperature;
+    private final List<String> pests;
 
     PlantType(double minWaterLevel, double maxWaterLevel, double lowWaterThreshold,
-              double minTemperature, double maxTemperature) {
+              double minTemperature, double maxTemperature, List<String> pests) {
         this.minWaterLevel = minWaterLevel;
         this.maxWaterLevel = maxWaterLevel;
         this.lowWaterThreshold = lowWaterThreshold;
         this.minTemperature = minTemperature;
         this.maxTemperature = maxTemperature;
+        this.pests = pests;
     }
 
     public double getMinWaterLevel() {
@@ -38,5 +44,9 @@ public enum PlantType {
 
     public double getMaxTemperature() {
         return maxTemperature;
+    }
+
+    public List<String> getPests() {
+        return pests;
     }
 }
